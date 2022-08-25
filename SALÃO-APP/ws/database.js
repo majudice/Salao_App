2bac97ad1e5fc3e4c8887d98185d6b8af315765f
majudice <mariana-developer@outlook.com>
2022-08-25 14:30:57 -0300

    commit-02
    arquivo.js

--- SALÃO-APP/ws/database.js
+++ SALÃO-APP/ws/database.js
@@ -1,9 +1,5 @@
 const mongoose  = require('mongoose');
-const URI = 'mongodb+srv://marianajudice:<Smnj070916>@clusterdev.gp5lojt.mongodb.net/?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://marianajudice:M85DlEpMxLDEip9C@clusterdev.gp5lojt.mongodb.net/salao-conceito?retryWrites=true&w=majority';
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
 
 mongoose.connect(URI).then(() => console.log('DB is Up!')).catch(() => console.log(err));
---
+++ SALÃO-APP/ws/src/routes/salao.routes.js
@@ -0,0 +1,36 @@
+const express =  require('express');
+const router = express.Router();
+const Salao = require('../models/salao')
+const Servico  = require('../models/servico')
+
+router.post('/', async (req, res) => {
+try {
+
+const salao = await new Salao(req.body).save();
+res.json({ salao});
+
+} catch(err){
+   res.json({ error: true, message: err.message}) 
+}
+});
+
+router.get('/servicos/:salaoId', async (req, res) => {
+try{
+const { salaoId } = req.params;
+const servicos = await Servico.find({
+ salaoId,
+ status:'A'  
+}).selet('_id titulo');
+
+/* [{label: 'Serviço', value: '123123123'}]  */
+res.json({
+   servicos: servicos.map(s => ({ label: s.titulo , value:s._id })),
+});
+
+}catch (err) {
+   res.json({ error: true, message: err.message});
+}
+
+})
+
+module.exports = router;
